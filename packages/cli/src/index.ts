095c0bc5203fcc5562e9c8d093bb35df7d23797d
codehz <git@hertz.moe>
2023-01-28 00:37:30 +0800

    add generate command

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,12 +1,26 @@
 #!/usr/bin/env node
-import { run, command, positional, option, subcommands } from 'cmd-ts';
+import {
+  run,
+  command,
+  positional,
+  option,
+  subcommands,
+  flag,
+  boolean,
+} from 'cmd-ts';
 import {
   GraphQLFile,
   ReadOnlyDatabaseFile,
   OutputStream,
 } from './lib/cli-helper.js';
 import { inferSchemaFromDatabase } from './lib/infer.js';
-import { generateSqlInitialMigration } from '@qlite/core';
+import {
+  filterOutBuiltinDefinitions,
+  generateRootTypes,
+  generateSqlInitialMigration,
+} from '@qlite/core';
+import { mergeSchemas } from '@graphql-tools/schema';
+import { printSchema } from 'graphql';
 
 const output = option({
   type: OutputStream,
@@ -42,9 +56,37 @@ const infer = command({
   },
 });
 
+const generate = command({
+  name: 'generate',
+  args: {
+    input: positional({
+      type: GraphQLFile,
+    }),
+    output,
+    stripDirectives: flag({
+      type: boolean,
+      long: 'strip-directives',
+      short: 's',
+    }),
+  },
+  handler(args) {
+    const [extended_schema, typedefs] = generateRootTypes(args.input);
+    const schema = mergeSchemas({
+      schemas: [args.input, extended_schema],
+      typeDefs: [typedefs],
+    });
+    if (args.stripDirectives) {
+      const mapped = filterOutBuiltinDefinitions(schema);
+      args.output.write(printSchema(mapped) + '\n');
+    } else {
+      args.output.write(printSchema(schema) + '\n');
+    }
+  },
+});
+
 const app = subcommands({
   name: 'qlite',
-  cmds: { init, infer },
+  cmds: { init, infer, generate },
 });
 
 run(app, process.argv.slice(2));
